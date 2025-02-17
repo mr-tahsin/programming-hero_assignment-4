1b06cfe644c716e3bfaffd841bdeb20d4391b524
Tahsin <tahsinfaruque@Tahsins-MacBook-Air.local>
2025-02-17 22:07:20 +0600

    first

---
+++ p.js
@@ -0,0 +1,40 @@
+// function basicFunction() {
+//   console.log("Hello, this is basic function");
+// }
+// basic();
+
+// function parameterFunction(name) {
+//   console.log(`My name is ${name}`);
+// }
+// parameterFunction("Tahsin");
+
+// function returnFunction(a, b, c) {
+//   return a + b - c;
+// }
+// console.log(`Simplification: ${returnFunction(5, 5, 2)}`);
+
+// function parameterFunction(name) {
+//   console.log("Her name is " + name);
+// }
+// parameterFunction("Alisha");
+
+// const person = {
+//   name: "Alice", // Value stored in the box
+//   greet: function () {
+//     // Function stored in the box
+//     console.log("Hello, my name is " + this.name);
+//   },
+// };
+// person.greet();
+
+//swapping
+// let a = 4;
+// let b = 6;
+// console.log(a);
+// console.log(b);
+
+// const temp = a;
+// a = b;
+// b = temp;
+// console.log(a);
+// console.log(b);
